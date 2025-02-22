9b11f4529f765fa914d0efd1b437686f7f3ce44a
Mark Scheuerell <scheuerl@uw.edu>
2025-02-22 08:29:13 -0800

    rebuilt website

---
+++ docs/lectures/week_05/fish549_db_sandbox.R
@@ -0,0 +1,28 @@
+library(DBI)
+library(RPostgres)
+
+## from Jennifer
+# Hostname: p.nxh57cg2izb6dbv27dykuuxztq.db.postgresbridge.com
+# Port: 5432
+# Database: postgres
+# Username: postgres
+# Password: FGE9jBmVIuizl0ImuR9N9rPer5hXAfuIifvkAazJC59EG5zhf8bIZUU3LTrN13Eo
+
+conn <- dbConnect(drv = Postgres(), 
+                  dbname = "postgres", 
+                  host = "p.nxh57cg2izb6dbv27dykuuxztq.db.postgresbridge.com", 
+                  port = "5432", 
+                  user = "postgres", 
+                  password = "FGE9jBmVIuizl0ImuR9N9rPer5hXAfuIifvkAazJC59EG5zhf8bIZUU3LTrN13Eo")
+
+dbListTables(conn)
+
+
+#### tables ####
+
+library(dplyr)
+library(dbplyr)
+
+
+#### queries ####
+
