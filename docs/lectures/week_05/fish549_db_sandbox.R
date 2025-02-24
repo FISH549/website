cc7ecba782e2c1936a1b77b7b7d58fdc089762d1
Mark Scheuerell <scheuerl@uw.edu>
2025-02-23 17:27:06 -0800

    rebuilt site

--- docs/lectures/week_05/fish549_db_sandbox.R
+++ docs/lectures/week_05/fish549_db_sandbox.R
@@ -1,5 +1,9 @@
 library(DBI)
-library(RPostgres)
+library(dplyr)
+library(dbplyr)
+library(odbc)
+
+
 
 ## from Jennifer
 # Hostname: p.nxh57cg2izb6dbv27dykuuxztq.db.postgresbridge.com
@@ -8,21 +12,45 @@ library(RPostgres)
 # Username: postgres
 # Password: FGE9jBmVIuizl0ImuR9N9rPer5hXAfuIifvkAazJC59EG5zhf8bIZUU3LTrN13Eo
 
-conn <- dbConnect(drv = Postgres(), 
-                  dbname = "postgres", 
-                  host = "p.nxh57cg2izb6dbv27dykuuxztq.db.postgresbridge.com", 
-                  port = "5432", 
-                  user = "postgres", 
-                  password = "FGE9jBmVIuizl0ImuR9N9rPer5hXAfuIifvkAazJC59EG5zhf8bIZUU3LTrN13Eo")
+install.packages("RPostgreSQL")
+require("RPostgreSQL")
+# this completes installing packages
 
-dbListTables(conn)
+# now start creating connection
+con <- dbConnect(dbDriver("PostgreSQL"),
+                 dbname = "postgres",
+                 host = "p.nxh57cg2izb6dbv27dykuuxztq.db.postgresbridge.com",
+                 port = 5432,
+                 user = "postgres",
+                 password = "FGE9jBmVIuizl0ImuR9N9rPer5hXAfuIifvkAazJC59EG5zhf8bIZUU3LTrN13Eo")
+# this completes creating connection
+
+# get all the tables from connection
+dbListTables(con)
 
 
-#### tables ####
-
-library(dplyr)
-library(dbplyr)
 
 
-#### queries ####
+## from here
+## https://solutions.posit.co/connections/db/best-practices/drivers/#connecting-to-a-database-in-r
+con <- dbConnect(
+  odbc::odbc(),
+  driver = dbDriver("PostgreSQL"),
+  database = "test_db",
+  uid    = Sys.getenv("DB_USER"),
+  pwd    = Sys.getenv("DB_PASSWORD"),
+  host   = "localhost",
+  port   = 5432
+)
+
+
+## from here
+## https://solutions.posit.co/connections/db/databases/postgresql/
+con <- DBI::dbConnect(odbc::odbc(),
+                      Driver   = "PostgreSQL Driver",
+                      Server   = "p.nxh57cg2izb6dbv27dykuuxztq.db.postgresbridge.com",
+                      Database = "postgres",
+                      UID      = "postgres", # rstudioapi::askForPassword("userID"),
+                      PWD      = "FGE9jBmVIuizl0ImuR9N9rPer5hXAfuIifvkAazJC59EG5zhf8bIZUU3LTrN13Eo", # rstudioapi::askForPassword("pwd"),
+                      Port     = 5432)
 
